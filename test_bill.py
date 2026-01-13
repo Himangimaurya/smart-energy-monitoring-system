9c2a3ff9090efc916e023572fb61bd34ac06bf37
Himangimaurya <himangimaurya0@gmail.com>
2026-01-13 18:55:32 +0530

    Ignore test files

--- test_bill.py
+++
@@ -1,9 +0,0 @@
-from analytics.bill_prediction import BillPredictor
-
-bill = BillPredictor()
-
-energy_values = [10, 25.5, 72.3]
-
-for energy in energy_values:
-    cost = bill.calculate_bill(energy)
-    print(f"Energy: {energy} kWh → Bill: ₹{cost}")
--- test_sensor.py
+++
@@ -1,8 +0,0 @@
-from sensors.sensor_simulator import SensorSimulator
-
-sensor = SensorSimulator()
-
-for i in range(5):
-    voltage = sensor.get_voltage()
-    current = sensor.get_current()
-    print(f"Voltage: {voltage} V | Current: {current} A")
